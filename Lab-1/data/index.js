33c13bb3b889f8d11eb6cdf95b6285ec49ae38d5
Puneet Sharma <puneet.s2004@gmail.com>
2017-09-04 14:48:07 -0400

    fix header

--- Lab-1/data/index.js
+++ Lab-1/data/index.js
@@ -1,4 +1,9 @@
-﻿const taskData = require("./tasks");
+﻿/*
+ * @Author : Puneet Sharma 
+ * @Copyright (c) 2017, Stevens Institute of Technology 
+ * @Created: 2017-09-03
+ */
+const taskData = require("./tasks");
 
 module.exports = {
     tasks: taskData
