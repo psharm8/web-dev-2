1bd8556308366a21546df7cc064276d83ddb5d39
Puneet Sharma <puneet.s2004@gmail.com>
2017-09-11 22:00:46 -0400

    Lab2 infra

---
+++ Lab-2/app.js
@@ -0,0 +1,20 @@
+/*
+ * @Author : Puneet Sharma 
+ * @Copyright (c) 2017, Stevens Institute of Technology 
+ * @Created: 2017-09-11
+ */
+
+const express = require("express");
+const pub = express.static(__dirname + '/public');
+let app = express();
+app.use("/public", pub);
+app.get("/", (req, res)=>{
+    res.sendFile("views/static/index.html", {root: __dirname });
+});
+app.use("*", (req, res)=>{
+    res.sendStatus(404);
+});
+app.listen(3000, () => {
+    console.log("We've now got a server!");
+    console.log("Your routes will be running on http://localhost:3000");
+});
