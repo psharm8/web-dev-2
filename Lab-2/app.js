07fb9c337edf2dc73a0b17f83ba2dad4a66b2f91
Puneet Sharma <puneet.s2004@gmail.com>
2017-09-12 20:25:23 -0400

    add error page

--- Lab-2/app.js
+++ Lab-2/app.js
@@ -8,11 +8,15 @@ const express = require("express");
 const pub = express.static(__dirname + '/public');
 let app = express();
 app.use("/public", pub);
-app.get("/", (req, res)=>{
-    res.sendFile("views/static/index.html", {root: __dirname });
+app.get("/", (req, res) => {
+    res.sendFile("views/static/index.html", {
+        root: __dirname
+    });
 });
-app.use("*", (req, res)=>{
-    res.sendStatus(404);
+app.use("*", (req, res) => {
+    res.status(404).sendFile("views/static/error.html", {
+        root: __dirname
+    });
 });
 app.listen(3000, () => {
     console.log("We've now got a server!");
